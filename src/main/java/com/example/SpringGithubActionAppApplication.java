57e6f5a532dc2f1b07d4aaede70a9b9d6ca0da36
spirometry <spirometry@naver.com>
2026-06-29 15:22:48 +0900

    commit 0.0.1

---
+++ src/main/java/com/example/SpringGithubActionAppApplication.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestController
+@SpringBootApplication
+public class SpringGithubActionAppApplication {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @GetMapping("/")
+    public String index(){
+        return appName + "-" + appVersion;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGithubActionAppApplication.class, args);
+    }
+
+}
